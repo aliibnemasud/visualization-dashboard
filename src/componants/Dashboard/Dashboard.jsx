5fc49703d5f1ddbe0c9d36622e6798e2c673f6b2
aliibnemasud <mdmasudrony@gmail.com>
2022-09-21 09:07:45 +0600

    first commit

---
+++ src/componants/Dashboard/Dashboard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="drawer drawer-mobile">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content m-12">
+                {/* <!-- Page content here --> */}
+                <h1 className="text-2xl">Dashboard</h1>
+
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-80 bg-slate-100 text-base-content">
+                    <h1 className="text-2xl mb-5">Visualization Dashboard</h1>
+                    {/* <!-- Sidebar content here --> */}
+                    <li className=' bg-slate-200 mb-2 rounded-sm'><a>Chart</a></li>
+                    <li className=' bg-slate-200 mb-2 rounded-sm'><a>D3.js</a></li>
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
