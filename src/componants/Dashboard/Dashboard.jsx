f061bd25bf22b3777db1421ef82080401fa9e641
Ali Ibne masud <mdmasudrony@gmail.com>
2022-10-11 11:10:00 +0600

    date added

--- src/componants/Dashboard/Dashboard.jsx
+++ src/componants/Dashboard/Dashboard.jsx
@@ -2,16 +2,38 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Charts from '../Chart/Charts';
 
-const Dashboard = () => {    
+const Dashboard = () => {
+
+    const today = new Date();
+
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+    const timeAMPM = (date) => {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+
+    const time = timeAMPM(today);
+
+
 
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content m-12">
                 {/* <!-- Page content here --> */}
-                <h1 className="text-2xl">Dashboard</h1>
+                <div className='flex justify-between mx-10'>
+                    <h1 className="text-2xl">Dashboard</h1>
+                    <h2>{date} | {time}</h2>
+                </div>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                <Outlet/>
+                <Outlet />
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
